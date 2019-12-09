0fc125c21d315620fc4fb3e9bc5fb791d99b584d
tobiascr <tobiascrw@gmail.com>
2019-12-09 16:46:35 +0100

    modified:   test_engine_vs_engine.py

--- test_engine_vs_engine.py
+++ test_engine_vs_engine.py
@@ -1,9 +1,6 @@
 
 from engine import EngineInterface as EngineInterface1
-from engine import GameState as GameState1
-from engine import reset_transposition_table
 from test_engine import EngineInterface as EngineInterface2
-from test_engine import GameState as GameState2
 import time
 
 engine_interface_1 = EngineInterface1(3)
@@ -11,7 +8,7 @@ engine_interface_1.name = "engine"
 engine_interface_2 = EngineInterface2(3)
 engine_interface_2.name = "test_engine"
 
-def game(engine1, game_state1, engine2, game_state2, print_move_times=False):
+def game(engine1, engine2, print_move_times=False):
     """engine1 and engine2 are instances of EngineInterface.
     engine1 makes the first move.
     Return (result, engine1 time, engine2 time)
@@ -25,34 +22,34 @@ def game(engine1, game_state1, engine2, game_state2, print_move_times=False):
     total_time_engine_2 = 0
     while True:
         t0 = time.perf_counter()
-        column_number = engine1.engine_move(game_state1)
+        column_number = engine1.engine_move()
         t1 = time.perf_counter()
         total_time_engine_1 += t1 - t0
         if print_move_times:
             print(engine1.name + ": Move time:", t1 - t0, "s")
-        game_state1.make_move(column_number)
-        game_state2.make_move(column_number)
-        if engine1.four_in_a_row(game_state1):
+        engine1.make_move(column_number)
+        engine2.make_move(column_number)
+        if engine1.four_in_a_row():
             print_total_times()
             print(engine1.name + " wins" + "\n")
             return (1, total_time_engine_1, total_time_engine_2)
-        if game_state1.number_of_moves == 42:
+        if engine1.draw():
             print_total_times()
             print("Draw\n")
             return (0, total_time_engine_1, total_time_engine_2)
         t0 = time.perf_counter()
-        column_number = engine2.engine_move(game_state2)
+        column_number = engine2.engine_move()
         t1 = time.perf_counter()
         total_time_engine_2 += t1 - t0
         if print_move_times:
             print(engine2.name + ": Move time:", t1 - t0, "s")
-        game_state1.make_move(column_number)
-        game_state2.make_move(column_number)
-        if engine2.four_in_a_row(game_state2):
+        engine1.make_move(column_number)
+        engine2.make_move(column_number)
+        if engine2.four_in_a_row():
             print_total_times()
             print(engine2.name + " wins" + "\n")
             return (2, total_time_engine_1, total_time_engine_2)
-        if game_state2.number_of_moves == 42:
+        if engine2.draw():
             print_total_times()
             print("Draw\n")
             return (0, total_time_engine_1, total_time_engine_2)
@@ -67,12 +64,11 @@ def games(engine1, engine2, number_of_games, print_move_times=False):
 
     draws = 0
     for n in range(1, number_of_games + 1):
-        reset_transposition_table()
         print("Game", n)
+        engine_interface_1.new_game()
+        engine_interface_2.new_game()
         if n % 2:
-            game_state1 = GameState1()
-            game_state2 = GameState2()
-            (result, t1, t2) = game(engine1, game_state1, engine2, game_state2, print_move_times)
+            (result, t1, t2) = game(engine1, engine2, print_move_times)
             if result == 1:
                 engine1_wins += 1
             elif result == 2:
@@ -82,9 +78,7 @@ def games(engine1, engine2, number_of_games, print_move_times=False):
             engine1_time += t1
             engine2_time += t2
         else:
-            game_state1 = GameState1()
-            game_state2 = GameState2()
-            (result, t2, t1) = game(engine2, game_state2, engine1, game_state1, print_move_times)
+            (result, t2, t1) = game(engine2, engine1, print_move_times)
             if result == 1:
                 engine2_wins += 1
             elif result == 2:
@@ -98,4 +92,4 @@ def games(engine1, engine2, number_of_games, print_move_times=False):
     print(engine1.name + " total time:", engine1_time, "s")
     print(engine2.name + " total time:", engine2_time, "s")
 
-games(engine_interface_1, engine_interface_2, 40, False)
+games(engine_interface_1, engine_interface_2, 1, False)
